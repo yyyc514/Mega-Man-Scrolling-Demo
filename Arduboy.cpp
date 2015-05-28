7ddbc115182373fdc1176aa6f5b423314864c16f
Josh Goebel <dreamer3@gmail.com>
2015-05-27 21:40:19 -0400

    more inc outside the conditional

--- Arduboy.cpp
+++ Arduboy.cpp
@@ -533,14 +533,15 @@ void Arduboy::drawBitmap(int16_t x, int16_t y, const uint8_t *bitmap, int16_t w,
         if (iColx > (WIDTH-1)) break;
         if (iColx > 0) {
           if (bRow >= 0) {
-            if (color) this->sBuffer[ofs++]  |= pgm_read_byte(bofs++) << yOffset;
-            else this->sBuffer[ofs++]  &= ~(pgm_read_byte(bofs++) << yOffset);
+            if (color) this->sBuffer[ofs]  |= pgm_read_byte(bofs++) << yOffset;
+            else this->sBuffer[ofs]  &= ~(pgm_read_byte(bofs++) << yOffset);
           }
           if (yOffset) {
             if (color) this->sBuffer[ ((bRow+1)*WIDTH) + iColx  ] |= pgm_read_byte(bitmap+(a*w)+iCol) >> (8-yOffset);
             else this->sBuffer[ ((bRow+1)*WIDTH) + iColx  ] &= ~(pgm_read_byte(bitmap+(a*w)+iCol) >> (8-yOffset));
           }
         }
+        ofs++;
       }
     }
   }
